7e22773a65eb82d1fbf66e4d0368d549a680f37a
mohamed mostafa <muhammed.moustafa.ahmed@gmail.com>
2024-04-10 13:27:19 +0200

    start project app

--- crowdFunding/crowdFunding/settings.py
+++ crowdFunding/crowdFunding/settings.py
@@ -80,6 +80,20 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'mysql.connector.django',
+#         'NAME': 'django_crowd',
+#         'USER': 'os44',
+#         'PASSWORD': 'iti',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#         }
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
