a8acf52834ce34f762f862dc8305c475b3f0c633
mohamed mostafa <muhammed.moustafa.ahmed@gmail.com>
2024-04-10 14:03:31 +0200

    start project model

--- crowdFunding/project/models.py
+++ crowdFunding/project/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class project(models.Model):
+    title = models.CharField(max_length=200 , unique=True)
+    details = models.TextField(default="No details")
+
+
+
