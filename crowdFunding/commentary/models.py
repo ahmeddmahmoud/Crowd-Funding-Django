8e7412888c4e029d1f2f144da81626dc6932d6bc
ahmeddmahmoud <ahmed.mahmoudd.elshahat@gmail.com>
2024-04-11 23:13:55 +0200

    fixing bugs

--- crowdFunding/commentary/models.py
+++ crowdFunding/commentary/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from project.models import Project 
-from users.models import User
+from users.models import CustomUser
 
 
 class Comment(models.Model):
     content=models.CharField(max_length=500)
     project = models.ForeignKey(Project,on_delete=models.CASCADE,related_name='comments')
-    user =models.ForeignKey(User,on_delete=models.CASCADE,related_name='comments')
+    user =models.ForeignKey(CustomUser,on_delete=models.CASCADE,related_name='comments')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
