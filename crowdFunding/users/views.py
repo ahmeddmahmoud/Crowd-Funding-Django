801975db5f7bfd17a4745e3665b22fa3ff5a8855
ahmeddmahmoud <ahmed.mahmoudd.elshahat@gmail.com>
2024-04-15 08:57:55 +0200

    adding user views

--- crowdFunding/users/views.py
+++ crowdFunding/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from users.forms import RegistrationForm
+from users.models import CustomUser
 
 
 def index(request):
@@ -23,4 +24,15 @@ def create_user(request):
     return render(request,'users/register.html',{'form': form})
 
 def user_details(request):
+    CustomUser.objects.get(id=0)
+
     return render(request,'users/user_details.html' )
+
+def user_home(request):
+    pass
+
+def user_projects(request):
+    return render(request,'users/user_projects.html' )
+
+def user_donations(request):
+    return render(request,'users/user_donations.html' )
