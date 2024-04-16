8342ea4c66638a6e9794fdd8c8a33936f94c8ebf
mohamed awad <mohamed.awad.elgammal@gmail.com>
2024-04-16 14:28:55 +0200

    add

--- crowdFunding/commentary/models.py
+++ crowdFunding/commentary/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from project.models import Project
-# from users.models import User
+from users.models import User
 
 
 class Comment(models.Model):
     text=models.TextField()
     rate= models.IntegerField(default=0)
     project = models.ForeignKey(Project,on_delete=models.CASCADE,related_name='comments')
-    # user =models.ForeignKey(User,on_delete=models.CASCADE,related_name='comments')
+    user =models.ForeignKey(User,on_delete=models.CASCADE,related_name='comments')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -17,7 +17,7 @@ class Comment(models.Model):
 class Report(models.Model):
     reason = models.CharField(max_length=500)
     status = models.CharField(max_length=25)
-    # user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reports')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reports')
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='reports')
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='reports', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
