4ad8a8e451694399f0703d054809d92e54383087
TalaatEysa <talaateysa@outlook.com>
2024-04-17 02:10:13 +0200

    adding email activation logic in views

--- crowdFunding/users/views.py
+++ crowdFunding/users/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .forms import UserRegistrationForm
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout,get_user_model
 from users.models import User
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.core.mail import EmailMessage
+from typing import Protocol
+from .tokens import account_activation_token
 
 
 def index(request):
@@ -20,10 +28,48 @@ def login_form(request):
             user = authenticate(request, email=email, password=password)
             if user is not None:
                 login(request, user)
+                messages.success(request, f"Hello {user.first_name}! You have been logged in")
                 url = reverse('user.details', args=[user.id])
                 return redirect(url)
 
     return render(request, 'users/login.html', {'form': form})
+
+
+def activate(request, uidb64, token):
+    User = get_user_model()
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except:
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+
+        messages.success(request, "Thank you for your email confirmation. Now you can login your account.")
+        return redirect('user.login')
+    else:
+        messages.error(request, "Activation link is invalid!")
+
+    return redirect('index')
+
+
+def activate_email(request, user, to_email):
+    mail_subject = "Activate your user account."
+    message = render_to_string("template_activate_account.html", {
+        'user': user.first_name,
+        'domain': get_current_site(request).domain,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': account_activation_token.make_token(user),
+        "protocol": 'https' if request.is_secure() else 'http'
+    })
+    email = EmailMessage(mail_subject, message, to=[to_email])
+    if email.send():
+        messages.success(request, f'Dear {user.first_name}!, please go to you email {to_email}! inbox and click on \
+                  received activation link to confirm and complete the registration. Note!: Check your spam folder.')
+    else:
+        messages.error(request, f'Problem sending email to {to_email}, check if you typed it correctly.')
 
 
 def register(request):
@@ -31,7 +77,8 @@ def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            user=form.save()
+            activate_email(request, user, form.cleaned_data.get('email'))
             login_url = reverse("index")
             return redirect(login_url)
     return render(request, 'users/register.html', {'form': form})
