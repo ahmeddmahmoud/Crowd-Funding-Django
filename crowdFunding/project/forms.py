0198e765b8c01b500ff00ba5db29bd83834d9946
TalaatEysa <talaateysa@outlook.com>
2024-04-18 06:19:45 +0200

    adding featured projects model and form

--- crowdFunding/project/forms.py
+++ crowdFunding/project/forms.py
@@ -1,22 +1,32 @@
 from django import forms
-from project.models import Project,Tag,Category,Donation
+from project.models import Project,Tag,Category,Donation,FeaturedProject
+
 
 class ProjectModelForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ('title', 'details', 'total_target', 'start_date', 'end_date','category', 'tag')
 
+
 class CategoryModelForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = '__all__'
+
 
 class TagModelForm(forms.ModelForm):
     class Meta:
         model = Tag
         fields = '__all__'
 
+
 class DonationModelForm(forms.ModelForm):
     class Meta:
         model = Donation
         fields = ('donation',)
+
+
+class FeaturedProjectForm(forms.ModelForm):
+    class Meta:
+        model = FeaturedProject
+        fields = ['project']
