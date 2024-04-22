6e19021bb9abeec43dc0eec9145ae2ec5bdfd2b3
TalaatEysa <talaateysa@outlook.com>
2024-04-22 23:13:53 +0200

    adding validation for tag and category

--- crowdFunding/project/forms.py
+++ crowdFunding/project/forms.py
@@ -26,17 +26,27 @@ class CategoryModelForm(forms.ModelForm):
         model = Category
         fields = '__all__'
 
-        def clean_name(self):
-            name = self.cleaned_data.get('name')
-            if name is None or len(name) < 3:
-                raise forms.ValidationError('Name must be provided and have a length greater than 3 characters')
-            return name
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        if not name.isalpha():
+            raise forms.ValidationError('Category name must be alphabetic')
+        if name is None or len(name) < 3:
+            raise forms.ValidationError('Category name should be at least 3 characters')
+        return name
 
 
 class TagModelForm(forms.ModelForm):
     class Meta:
         model = Tag
         fields = '__all__'
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        if not name.isalpha():
+            raise forms.ValidationError('Tag name must be alphabetic')
+        if name is None or len(name) < 3:
+            raise forms.ValidationError('Tag name should be at least 3 characters')
+        return name
 
 
 class DonationModelForm(forms.ModelForm):
