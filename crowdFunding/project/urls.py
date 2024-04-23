2bfb0a99a7bb1b9573bb80378c5374e94ba39c32
TalaatEysa <talaateysa@outlook.com>
2024-04-23 22:48:15 +0200

    adding authorization for project

--- crowdFunding/project/urls.py
+++ crowdFunding/project/urls.py
@@ -1,11 +1,11 @@
-from project.views import (hello, create_project_model_form,
+from project.views import ( create_project_model_form,
     list_project, cancel_project,project_show, donate_project, edit_project,
-                            show_category,add_images,clear_images)
+                            add_images,clear_images)
 from commentary.views import add_comment
 from django.urls import path
 
 urlpatterns = [
-    path("hello", hello, name= "hello"),
+    # path("hello", hello, name= "hello"),
     path('<int:id>/comment', add_comment, name='project.comment'),
     path('createproject', create_project_model_form, name='project.createmodel'),
     path('<int:id>', project_show, name='project.show'),
@@ -13,7 +13,6 @@ urlpatterns = [
     path('', list_project, name='project.list'),
     path('donate/<int:id>', donate_project, name='project.donate'),
     path('<int:id>/edit', edit_project, name='project.edit'),
-    path('category/<int:id>/show', show_category, name='category.show'),
     path('<int:id>/addimage', add_images, name='project.addimage'),
     path('<int:id>/clearimage', clear_images, name='project.clearimage'),
     
