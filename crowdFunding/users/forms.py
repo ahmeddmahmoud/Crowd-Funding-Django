d57e06991531daf042d318398bc9e2ec9206913d
TalaatEysa <talaateysa@outlook.com>
2024-04-23 20:58:16 +0200

    modyfing password in edit user by admin

--- crowdFunding/users/forms.py
+++ crowdFunding/users/forms.py
@@ -255,7 +255,7 @@ class UserEditFormByAdmin(forms.ModelForm):
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
-        if password1 and password1 != password2:
+        if (password1 and password1 != password2) or (password2 and password2 != password1):
             raise forms.ValidationError("Passwords don't match")
         return password2
 
